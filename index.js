de73e767918a06cb9c0d5bdebbd820d89af0b83b
Sahariar Mondal <sahariarmondal445@gmail.com>
2025-07-28 16:13:40 +0530

    Ignore node_modules and .env files

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+})
